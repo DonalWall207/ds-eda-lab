6f3593005cd95352210ca3af667bc181cd90a3e0
Donal Wall <91415175+DonalWall207@users.noreply.github.com>
2024-11-08 13:25:19 +0000

    Started Fan out for S3 image upload.

--- bin/eda-app.ts
+++ bin/eda-app.ts
@@ -7,6 +7,8 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3n from "aws-cdk-lib/aws-s3-notifications";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as events from "aws-cdk-lib/aws-lambda-event-sources";
 
 const app = new cdk.App();
@@ -15,9 +17,18 @@ const edaStack = new EDAAppStack(app, "EDAStack", {
 });
 
 // Queue definition
-const queue = new sqs.Queue(edaStack, "ImgCreatedQueue", {
-  receiveMessageWaitTime: cdk.Duration.seconds(5),
+const imageProcessQueue = new sqs.Queue(edaStack, "img-created-queue", {
+  receiveMessageWaitTime: cdk.Duration.seconds(10),
 });
+
+// SNS Topic for new image notifications
+const newImageTopic = new sns.Topic(edaStack, "NewImageTopic", {
+  displayName: "New Image topic",
+});
+
+newImageTopic.addSubscription(
+  new subs.SqsSubscription(imageProcessQueue)
+);
 
 // Lambda function for processing images
 const processImageFn = new lambdanode.NodejsFunction(
@@ -36,14 +47,14 @@ const imagesBucket = new s3.Bucket(edaStack, "ImagesBucket", {
   removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
 
-// S3 --> SQS: Trigger queue on image creation in S3
+// S3 --> SNS --> SQS: Trigger SNS topic on image creation in S3
 imagesBucket.addEventNotification(
   s3.EventType.OBJECT_CREATED,
-  new s3n.SqsDestination(queue)
+  new s3n.SnsDestination(newImageTopic)
 );
 
 // SQS --> Lambda: Process new images in batches
-const newImageEventSource = new events.SqsEventSource(queue, {
+const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
   batchSize: 5,
   maxBatchingWindow: cdk.Duration.seconds(5),
 });
