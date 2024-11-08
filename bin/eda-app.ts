a1d45bed9358f1196876ac0bff08801aec3bee14
Donal Wall <91415175+DonalWall207@users.noreply.github.com>
2024-11-08 15:45:31 +0000

    Completed Fan out implementation - added emailing logic

--- bin/eda-app.ts
+++ bin/eda-app.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { EDAAppStack } from "../lib/eda-app-stack";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
@@ -10,15 +9,11 @@ import * as s3n from "aws-cdk-lib/aws-s3-notifications";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as events from "aws-cdk-lib/aws-lambda-event-sources";
+import * as iam from "aws-cdk-lib/aws-iam"; // Import IAM module
 
 const app = new cdk.App();
-const edaStack = new EDAAppStack(app, "EDAStack", {
+const edaStack = new cdk.Stack(app, "EDAStack", {
   env: { region: "eu-west-1" },
-});
-
-// Queue definition
-const imageProcessQueue = new sqs.Queue(edaStack, "img-created-queue", {
-  receiveMessageWaitTime: cdk.Duration.seconds(10),
 });
 
 // SNS Topic for new image notifications
@@ -26,26 +21,42 @@ const newImageTopic = new sns.Topic(edaStack, "NewImageTopic", {
   displayName: "New Image topic",
 });
 
-newImageTopic.addSubscription(
-  new subs.SqsSubscription(imageProcessQueue)
-);
+// Queue definition
+const imageProcessQueue = new sqs.Queue(edaStack, "img-created-queue", {
+  receiveMessageWaitTime: cdk.Duration.seconds(10),
+});
+
+const mailerQ = new sqs.Queue(edaStack, "mailer-queue", {  // Use edaStack instead of this
+  receiveMessageWaitTime: cdk.Duration.seconds(10),
+});
+
+newImageTopic.addSubscription(new subs.SqsSubscription(imageProcessQueue));
+newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
 // Lambda function for processing images
-const processImageFn = new lambdanode.NodejsFunction(
-  edaStack,
-  "ProcessImageFn",
-  {
-    runtime: lambda.Runtime.NODEJS_18_X,
-    entry: `${__dirname}/../lambdas/processImage.ts`,
-    timeout: cdk.Duration.seconds(15),
-    memorySize: 128,
-  }
-);
+const processImageFn = new lambdanode.NodejsFunction(edaStack, "ProcessImageFn", {
+  runtime: lambda.Runtime.NODEJS_18_X,
+  entry: `${__dirname}/../lambdas/processImage.ts`,
+  timeout: cdk.Duration.seconds(15),
+  memorySize: 128,
+});
+
+const mailerFn = new lambdanode.NodejsFunction(edaStack, "mailer-function", {  // Use edaStack instead of this
+  runtime: lambda.Runtime.NODEJS_16_X,
+  memorySize: 1024,
+  timeout: cdk.Duration.seconds(3),
+  entry: `${__dirname}/../lambdas/mailer.ts`,
+});
 
 // Define S3 bucket with notifications
 const imagesBucket = new s3.Bucket(edaStack, "ImagesBucket", {
   removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
+
+const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+  batchSize: 5,
+  maxBatchingWindow: cdk.Duration.seconds(5),
+}); 
 
 // S3 --> SNS --> SQS: Trigger SNS topic on image creation in S3
 imagesBucket.addEventNotification(
@@ -60,6 +71,19 @@ const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
 });
 
 processImageFn.addEventSource(newImageEventSource);
+mailerFn.addEventSource(newImageMailEventSource);
+
+mailerFn.addToRolePolicy(
+  new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    actions: [
+      "ses:SendEmail",
+      "ses:SendRawEmail",
+      "ses:SendTemplatedEmail",
+    ],
+    resources: ["*"],
+  })
+);
 
 // Permissions: Grant Lambda function read access to the S3 bucket
 imagesBucket.grantRead(processImageFn);
